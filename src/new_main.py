8e715f1df2ab44672ee0b6bab8e6f1e6d00f4114
sladowsk <kamil.sladowski@nokia.com>
2019-01-12 20:47:05 +0100

    refactor

--- src/new_main.py
+++ src/new_main.py
@@ -1,11 +1,9 @@
 from DrawingManager import DrawingManager
-from MultipleDrawingManager import MultipleDrawingManager
 from Game import Game
 from Mark import Mark
 
 
 d = DrawingManager()
-# d = MultipleDrawingManager()
 mark = Mark(d)
 game = Game(d, mark)
 game.play()
